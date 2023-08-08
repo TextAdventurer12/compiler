5db97cacd22e149846be449ebebdfdcec6d536d7
KermitNuggies <50683296+TextAdventurer12@users.noreply.github.com>
2023-08-08 00:31:32 +0000

    amogus

--- preProcessor.h
+++ preProcessor.h
@@ -6,11 +6,24 @@
 #include <string.h>
 #include "miscOps.h"
 
-char* pushActive(int regs);
-char* popActive(int regs);
-int getActiveRegs(char* line);
-void writePrologue(FILE* rdi, int allocSize, int regs);
-void writeEpilogue(FILE* rdi, int regs);
-void preProcessor(FILE* rdi, char* line);
+typedef int reg_key;
+typedef const char *string;
+
+// returns the string that will push all of the selected registers
+char* push_active(reg_key regs);
+// returns the string that will pop all of the selected registers
+char* pop_active(reg_key regs);
+// looks through the given string to detect the mentioned registers
+reg_key get_active_registers(string line);
+// writes the function prologue
+// dst -> the file that is written to
+// alloc_size -> the number of bytes that is allocated for use in the function
+// regs -> the reg_key representing the registers that should be prepared for the function
+void write_prologue(FILE* dst, size_t alloc_size, reg_key regs);
+// writes the function epilogue
+// dst -> the file that is written
+// regs -> the reg_key representing the registers that should be fixed after the function
+void write_epilogue(FILE* dst, reg_key regs);
+void preProcessor(FILE* rdi, string line);
 
 #endif
